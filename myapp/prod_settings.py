9a30cf15b640b57c38cc388c97ae928fa9b54169
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-06-08 15:24:39 +0500

    hot fix

--- myapp/prod_settings.py
+++ myapp/prod_settings.py
@@ -10,13 +10,20 @@ SECRET_KEY = 'o7yo95^hy3-_c*#!m+m&^nc_t=!(@17mw6p1_@s)&s27njp_v$'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'NAME': 'djangotest',
-        'USER': 'root',
-        'PASSWORD': '',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'mydatabase'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': '127.0.0.1',
+#         'NAME': 'djangotest',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'PORT': ''
+#     }
+# }
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
